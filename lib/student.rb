26b0760c2aa23bf92ee94d1ad38fcf6393e57f5e
Michael Carroll <carrollm2@gmail.com>
2020-01-11 03:47:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,17 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+
+  @@all = []
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+    @all << self
+  end
+
+
 end
