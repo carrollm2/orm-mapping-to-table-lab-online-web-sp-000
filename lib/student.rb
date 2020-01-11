67a419d19176828286a4478eb9e4632011fda83e
Michael Carroll <carrollm2@gmail.com>
2020-01-11 03:52:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,8 +13,15 @@ class Student
     @@all << self
   end
 
-  def self.all
-    @@all
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXIST students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER,
+      )
+      SQL
+    DB[:conn].execute(sql)
   end
 
 end
